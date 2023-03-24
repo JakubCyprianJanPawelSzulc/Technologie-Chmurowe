c98e0f3ee3619e7eab442226af581998bb318d86
Jakub Szulc <j.szulc.328@studms.ug.edu.pl>
2023-03-24 17:20:53 +0100

    lab4zad4

---
+++ lab4/zad4/skrypt.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+password="admin"
+
+volume_name="test_volume"
+
+temp_file=$(mktemp)
+
+docker run --rm -v $volume_name:/data alpine tar -czf $temp_file /data
+
+gpg --batch --passphrase "$password" --symmetric $temp_file
+
+rm $temp_file
+
+docker volume create $volume_name_encrypted
+
+docker run --rm -v $volume_name_encrypted:/data alpine sh -c "mkdir /data/decrypted; gpg --batch --passphrase '$password' --decrypt /data/encrypted.tar.gz.gpg | tar -xz -C /data/decrypted"
+
+docker run --rm -v $volume_name_encrypted:/data alpine ls /data/decrypted
+
+docker volume rm $volume_name_encrypted
+docker volume rm $volume_name
