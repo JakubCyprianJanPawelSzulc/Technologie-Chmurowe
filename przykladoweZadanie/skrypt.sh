c5aa1a2cb105153ad0f1a8f8c620949a83774789
Jakub Szulc <j.szulc.328@studms.ug.edu.pl>
2023-04-20 17:08:34 +0200

    .

--- przykladoweZadanie/skrypt.sh
+++ przykladoweZadanie/skrypt.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-GIT_REPO=https://github.com/JakubCyprianJanPawelSzulc/Projekt-Frontend-Development.git
-
-docker build --build-arg GIT_REPO=$GIT_REPO -t myapp-dev .
+GIT_REPO=$1
 
 docker network create app_network
 
-# docker run -d --name myapp-container -p 8080:3000 --network app_network myapp-dev
-docker run -d --name myapp-container -p 8080:3000 myapp-dev
-
-# docker run -d --name nginx-container -p 80:80 --network app_network -v /path/to/nginx.conf:/etc/nginx/nginx.conf nginx
-docker run -d --name nginx-container --network app_network -p 80:80 nginx
+docker build --build-arg GIT_REPO=$GIT_REPO -t myapp .
+docker run --name myapp --network app_network -p 8080:80 myapp
